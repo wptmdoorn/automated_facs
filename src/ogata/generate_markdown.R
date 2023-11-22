19f7fcdbdccd25a2d9c16e0a23fe00d7ea102ff7
William van Doorn <wptmdoorn@gmail.com>
2023-11-22 20:43:15 +0100

    include OGATA calculations

---
+++ src/ogata/generate_markdown.R
@@ -0,0 +1,25 @@
+library(flowCore)
+library(flowWorkspace)
+
+source("src/ogata/ogata_calculations.R")
+files <- list.files("data/fcs", pattern = "*.fcs", full.names = TRUE)
+
+for (filename in files) {
+    data <- flowCore::read.FCS(filename)
+    comp <- flowCore::keyword(data)[["SPILL"]]
+
+    data <- flowWorkspace::compensate(
+        data,
+        comp
+    )
+    transformList <- flowCore::estimateLogicle(data, channels = colnames(comp))
+    data <- flowWorkspace::transform(data, transformList)
+
+    rmarkdown::render("src/ogata/ogata.Rmd",
+        params = list(data = data, gates = process_ogata(data, filename)),
+        output_file = sprintf(
+            "../../output/ogata_%s.pdf",
+            tools::file_path_sans_ext(basename(filename))
+        )
+    )
+}
---
+++ src/ogata/ogata_calculations.R
@@ -0,0 +1,171 @@
+library(ggplot2)
+library(flowCore)
+library(flowViz)
+library(flowDensity)
+
+process_ogata <- function(data, file) {
+    channels <- c("V500-A", "SSC-A")
+
+    singlets <- flowDensity(data,
+        channels = c("FSC-A", "FSC-H"), position = c(F, F),
+        percentile = c(.99999, .99999), use.percentile = c(T, T),
+        ellip.gate = T, scale = .99
+    )
+
+    granu <- flowDensity(
+        obj = singlets, channels = c("V500-A", "SSC-A"),
+        position = c(TRUE, TRUE),
+        gates = c(1.5, mean(data@exprs[, "SSC-A"] - 7000)),
+        upper = TRUE,
+        percentile = c(.4, .5), use.percentile = c(T, T),
+    )
+
+    granu <- flowDensity(
+        obj = granu, channels = c("V500-A", "SSC-A"),
+        position = c(FALSE, TRUE),
+        percentile = c(0.9, 0.05), use.percentile = c(T, T),
+        ellip.gate = T,
+    )
+
+    ery <- flowDensity(
+        obj = singlets, channels = c("V500-A", "SSC-A"),
+        position = c(FALSE, FALSE),
+        ellip.gate = TRUE, scale = .95
+        # tinypeak.removal = c(0.1, 0.9)
+    )
+
+    lymph_and_mono <- flowDensity(
+        obj = singlets,
+        channels = channels,
+        position = c(TRUE, FALSE),
+        use.percentile = c(T, F),
+        percentile = c(.75, NA), gates = c(2.5, 50000),
+    )
+
+    # Second call to flowDensity
+    mono <- flowDensity(
+        obj = lymph_and_mono, channels = channels,
+        position = c(TRUE, TRUE), gates = c(FALSE, NA),
+        upper = c(NA, TRUE),
+        percentile = c(NA, 0.1)
+    )
+
+    mono <- flowDensity(
+        obj = mono, channels = channels,
+        position = c(TRUE, TRUE), gates = c(
+            lymph_and_mono@gates[1],
+            mono@gates[2]
+        ), ellip.gate = TRUE, scale = .99
+    )
+
+    lympho <- flowDensity(
+        obj = lymph_and_mono, channels = channels,
+        position = c(TRUE, FALSE), gates = c(0, NA),
+        ellip.gate = TRUE, scale = .975,
+    )
+
+    lympho <- flowDensity(
+        obj = lympho, channels = channels,
+        position = c(TRUE, FALSE), gates = c(
+            lymph_and_mono@gates[1],
+            lympho@gates[2]
+        ), ellip.gate = TRUE, scale = .99
+    )
+
+    blast.gate1 <- flowDensity(
+        obj = singlets, channels = c("PerCP-Cy5-5-A", "SSC-A"),
+        position = c(TRUE, FALSE),
+    )
+
+    b_inc <- ifelse(length(getPeaks(data,
+        channel = "PerCP-Cy5-5-A"
+    )$Peaks) == 2, 0, .5)
+
+    blast <- flowDensity(
+        obj = singlets, c("PerCP-Cy5-5-A", "SSC-A"),
+        position = c(TRUE, FALSE), gates = c(
+            blast.gate1@gates[1] + b_inc,
+            35000
+        ), ellip.gate = T,
+    )
+
+    low_ssc <- flowDensity(
+        obj = singlets,
+        channels = c("FSC-A", "SSC-A"),
+        position = c(TRUE, FALSE),
+        gates = c(8000, NA),
+    )
+
+    cd34_cd45_pos <- flowDensity(
+        obj = singlets,
+        channels = c("V500-A", "PerCP-Cy5-5-A"),
+        position = c(NA, TRUE),
+        percentile = c(NA, .975)
+    )
+
+    bprog <- flowDensity(
+        obj = cd34_cd45_pos,
+        channels = c("PerCP-Cy5-5-A", "APC-H7-A"),
+        position = c(TRUE, TRUE),
+        gates = c(.1, NA),
+    )
+
+    myeloblast <- flowDensity(
+        obj = cd34_cd45_pos,
+        channels = c("PerCP-Cy5-5-A", "APC-H7-A"),
+        position = c(TRUE, FALSE),
+        gates = c(.1, NA),
+    )
+
+    print(cd34_cd45_pos@cell.count)
+    print(myeloblast@cell.count)
+    print(bprog@cell.count)
+    print(singlets@cell.count)
+
+    ogata_myeloblast <- (myeloblast@cell.count / singlets@cell.count) * 100
+    ogata_bprog <- (bprog@cell.count / cd34_cd45_pos@cell.count) * 100
+    ogata_lymph_myeloblast <- (mean(exprs(lympho@flow.frame)[, "V500-A"],
+        na.rm = TRUE
+    ) / mean(exprs(myeloblast@flow.frame)[, "V500-A"], na.rm = TRUE))
+    ogata_granulo_lymph <- (mean(exprs(granu@flow.frame)[, "SSC-A"],
+        na.rm = TRUE
+    ) / mean(exprs(lympho@flow.frame)[, "SSC-A"], na.rm = TRUE))
+
+    total <- (ogata_myeloblast > 2) + (ogata_bprog < 5) +
+        (ogata_lymph_myeloblast <= 4 | ogata_lymph_myeloblast >= 7.5) +
+        (ogata_granulo_lymph <= 6)
+
+    ogata_table <- data.frame(
+        Parameter = c(
+            "Myeloblast (% of CD45+ cells)",
+            "B-progenitor-related cluster size (% of CD34+ cells)",
+            "Lymphocyte to myeloblast CD45 ratio",
+            "Granulocyte to lymphocyte SSC ratio",
+            "**Total score**"
+        ),
+        "Cut-off values" = c("> 2%", "< 5%", "<= 4 or >= 7.5", "<= 6", ">= 2"),
+        "Result" = c(
+            ogata_myeloblast,
+            ogata_bprog, ogata_lymph_myeloblast, ogata_granulo_lymph, total
+        )
+    )
+
+    # round each value in the Value column
+    ogata_table$Result <- round(ogata_table$Result, 2)
+
+    list(
+        "filename" = sprintf("Ogata score calculations (%s)", file),
+        "data" = data,
+        "singlets" = singlets,
+        "granu" = granu,
+        "ery" = ery,
+        "lympho" = lympho,
+        "mono" = mono,
+        "blast" = blast,
+        "low_ssc" = low_ssc,
+        "cd34_cd45_pos" = cd34_cd45_pos,
+        "bprog" = bprog,
+        "myeloblast" = myeloblast,
+        "ogata_table" = ogata_table
+    )
+}
